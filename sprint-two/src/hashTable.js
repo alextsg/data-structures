da19daa93e2662f9a366c7fc680d46cd0665c0b6
alextsg <alextsg@gmail.com>
2014-12-12 11:38:43 -0800

    Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[i] === undefined){
+     this._storage[i] = [];
+  }
+  this._storage[i].push(k);
+  this._storage[i].push(v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var result = null;
+  for(var j = 0;j < this._storage[i].length; j = j + 2){
+    if(this._storage[i][j] === k){
+      result = this._storage[i][j+1];
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+    for(var j = 0;j < this._storage[i].length; j = j + 2){
+    if(this._storage[i][j] === k){
+      this._storage[i][j] = null;
+      this._storage[i][j+1] = null;
+    }
+  }
 };
 
 
